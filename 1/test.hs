f89f1741e1e6568f6c1022166089de0034a556be
S. Matthew English <s.matthew.english@gmail.com>
2018-06-28 19:36:25 -0400

    :l baby

--- 1/test.hs
+++ 1/test.hs
@@ -1,4 +1,5 @@
 main = print (succ 5) >>
        print (max 13 666) >>
        print (succ 9 + max 5 4 + 1) >>
-       print ((succ 9) + (max 5 4) + 1)
+       print ((succ 9) + (max 5 4) + 1) >>
+       print (92 `div` 10)
