f5bff40a56405284f4904a0f482fc3d8ec1a67ad
S. Matthew English <s.matthew.english@gmail.com>
2018-06-28 21:02:48 -0400

    2018.06.28

--- 1/baby.hs
+++ 1/baby.hs
@@ -13,3 +13,27 @@ doubleSmallNumber0 x = if x > 100
 doubleSmallNumber1 x = (if x > 100 then x else x * 2) + 1
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+-- Using the `let` keyword to define a name in GHCi. 
+-- Entering `let a = 1` in GHCi is equivalent to
+-- writing `a = 1` in a script, then loading it with
+-- :l
+a = 1
+
+matt = [1, 2, 3, 4]
+hew = [9, 10, 11, 12]
+
+-- In Haskell, string are really just lists of characters.
+-- For example, the string "hello" is actually the same as 
+-- the list `['h', 'e', 'l', 'l', 'o']`. Because of this, we
+-- can use list functions on strings, which is really handy.
+eq1 = "hello"
+eq2 = ['h', 'e', 'l', 'l', 'o']
+
+number666 result = if eq1 == eq2 
+	then (putStrLn "True")
+	else (putStrLn "False")		
+
+
+
+
