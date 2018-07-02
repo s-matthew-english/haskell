b8a398932231d9653bc3d0f0db97165a59e4947d
S. Matthew English <s.matthew.english@gmail.com>
2018-07-01 23:58:06 -0400

    2018.07.01

---
+++ 3/bang.hs
@@ -0,0 +1,31 @@
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER SEVEN"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5!"
+
+faktorial :: Int -> Int
+faktorial 0 = 1
+faktorial n = n * faktorial (n - 1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+addVectors' :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors' (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+
+
+
+
